e05d0d7b1cdc7f1822f9fc625400f335c5301611
Mari <wourtex99@gmail.com>
2022-11-28 22:53:49 +0300

    feat: select theme

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sideMenu = document.querySelector('aside');
+const theme = document.querySelector(".theme");
+const sun = document.querySelector(".theme span:nth-child(1)");
+const moon = document.querySelector(".theme span:nth-child(2)");
 
 // Развернуть меню
 menuBtn.addEventListener('click', () => {
@@ -12,3 +15,9 @@ closeBtn.addEventListener('click', () => {
   sideMenu.style.display = 'none';
 });
 
+// Смена темы
+theme.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  sun.classList.toggle('active');
+  moon.classList.toggle('active');
+});
